68cfc09600faf1bd30df9bf5a9bd0ae65eb16e62
Rachel Kiarie <rachelnnk94@gmail.com>
2022-03-06 11:20:58 +0300

    Add validation for the gender dropdown element to ensure user selects gender.

--- js/script.js
+++ js/script.js
@@ -3,27 +3,32 @@
     var d = document.getElementById("DOB").value;
     var m = document.getElementById("month").value;
     var y = document.getElementById("year").value;
-    var fem = document.getElementById("female").value;
-    var mal = document.getElementById("male").value;
+    var s = document.getElementById("gender").value;
+    
     checkOK = "true";
     if (d<=0 || d>31) {
       alert("Please enter a valid date.");
       checkOK = "false";
-
     }
     if (m<=0 || m>12) {
       alert("Please enter a valid month.");
+      checkOK = "false";
+    }
+    if (gender.value == ""){
+      alert("Please select gender");
+      checkOK= "false";
     }
     if(checkOK == "false") {
       return false;
     } 
+    
     if (checkOK == "true") {
       // var century = Math.floor(y/100);
     // alert(century);
     // dow stands for day of the week
       // var dow = ( ( (century/4) -2*century-1) + ((5*y/4) ) + ((26*(m+1)/10)) + d ) % 7;
       // alert(dow);
-      alert (fem);
+      alert(s);
     }
     
   }
